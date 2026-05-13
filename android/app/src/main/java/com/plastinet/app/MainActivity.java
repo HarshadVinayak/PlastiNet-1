abc8f93936c81f20a2ade92b8bc942331c8642ae
Harish Ramamoorthy <harishramamoorthy@Harishs-MacBook-Pro.local>
2026-05-13 07:53:59 +0530

    Nuclear Reset: Deploy factory-clean Android project and definitive working APK

--- android/app/src/main/java/com/plastinet/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.plastinet.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
---
+++ android/app/src/main/java/com/plastinet/official/MainActivity.java
@@ -0,0 +1,5 @@
+package com.plastinet.official;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
